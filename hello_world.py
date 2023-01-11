99127f168e087be681bdffa76b5cc3a74863a6da
Malleswara Reddy Mula <malleswarareddy.coding@gmail.com>
2023-01-11 15:53:57 +0530

    hello world

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+import cherrypy
+import random
+import string
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello World!"
+
+    @cherrypy.expose
+    def generate(self):
+        return ''.join(random.sample(string.hexdigits, 8))
+
+if __name__ == "__main__":
+    cherrypy.quickstart(HelloWorld())
