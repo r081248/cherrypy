8c502de3c061f88add750920da0b369d3052a273
Malleswara Reddy Mula <malleswarareddy.coding@gmail.com>
2023-01-11 15:56:38 +0530

    fix: documentation

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,11 @@
 import cherrypy
 import random
 import string
-
+'''
+cherrypy matches exposed function name with resource
+ex: localhost:8080/ --> cherrypy webserves invokes index function
+    localhost:8080/generate --> cherrypy webserver invokes generate function
+'''
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
